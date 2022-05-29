a01d16a717681164dee7bbd663fa9e0b962fe8e3
Ferriol Pey Comas <ferriol73pey@gmail.com>
2022-05-29 17:36:54 +0200

    Added the scripts to train and predict

---
+++ predict.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import cv2
+import mediapipe as mp
+from time import time as t
+from hand import Hand
+
+import tensorflow as tf
+import numpy as np
+
+mp_hands = mp.solutions.hands
+
+MODEL = 'models/modelv1.hdf5'
+timeWindowSize = 30
+
+THRESHOLD = 0.80
+
+FPS = 20.0
+MINFREQ = (1/FPS)* 0.98 # Getting some margin just in case
+
+# Frames to store
+FRAMESSTORE = FPS
+# Store 20 points * 2 coordinates (x ,y) * FRAMESSTORE frames
+GESTURE_WINDOW_MAX_SIZE = int(20*2*FRAMESSTORE)
+
+def main():
+    # Loading the model
+    model = tf.keras.models.load_model(MODEL)
+    # For webcam input:
+    cap = cv2.VideoCapture(0)
+    timeWindow = []
+    gestureWindow=[]
+    with mp_hands.Hands( model_complexity=0,
+                         min_detection_confidence=0.5,
+                         min_tracking_confidence=0.5) as hands: #max_num_hands = 1
+        while cap.isOpened():
+            # Trying to get a more stable framerate
+            if (len (timeWindow)<1) or (t()-timeWindow[-1]>=MINFREQ ):
+                # Saves the times to get the framerate
+                timeWindow.append(t())
+                # If it is bigger than the accepted size just cut the extra part
+                if len(timeWindow)>timeWindowSize:
+                    timeWindow=timeWindow[-timeWindowSize:]
+
+                success, image = cap.read()
+                if not success:
+                    print('[INFO] Ignoring empty camera frame.')
+                    # If loading a video, use 'break' instead of 'continue'.
+                    continue
+
+                # To improve performance, optionally mark the image as not writeable to
+                # pass by reference.
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = hands.process(image)
+
+                # Draw the hand annotations on the image.
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                # list of found hands
+                handsList = []
+                if results.multi_hand_landmarks:
+                    for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
+                                                          results.multi_handedness):
+                        image_width, image_height = image.shape[1], image.shape[0]
+
+                        # Creates a custom object hand and append it to hands
+                        handsList.append(Hand(image_width, image_height,hand_landmarks, handedness))
+                        # Shows the found hand
+                        handsList[-1].drawHand(image)
+                        # Adds it to the window
+                        gestureWindow.extend(handsList[-1].normalized_landmarks)
+
+                        if len(gestureWindow)> GESTURE_WINDOW_MAX_SIZE:
+                            gestureWindow=gestureWindow[-GESTURE_WINDOW_MAX_SIZE:]
+                            prediction = model(np.array([gestureWindow]))[0]
+                            index_max = int(tf.argmax(prediction))
+                            # Saves the prediction as predicted label, probability
+                            prediction = [index_max,float(prediction[index_max])]
+                            #print(prediction)
+                            if prediction[1]>THRESHOLD:
+                                print(f'[INFO] identifyed {prediction[0]}')
+                                # Removes the window to avoid repetitions
+                                gestureWindow=[]
+
+                # Flip the image horizontally for a selfie-view display.
+                image =  cv2.flip(image, 1)
+                # Compute and write fps
+                l = len(timeWindow)
+                if l>1:
+                    avFPS =0
+                    for i in range(l-1,0,-1):
+                        avFPS += (timeWindow[i]-timeWindow[i-1])
+                    avFPS = l/avFPS
+                    # Write FPS
+                    text = f'{avFPS:.2f}fps'
+                    cv2.putText(image, text, (15, 25), cv2.FONT_HERSHEY_TRIPLEX, 1, ( 0, 0, 0), 1)
+
+                # Showing the image
+                cv2.imshow('MediaPipe Hands', image)
+
+                #wait and get the key pressed
+                key = cv2.waitKey(5)
+                if key & 0xFF == 27: # If ESC is pressed, exit
+                    break
+
+    cap.release()
+
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+DATA_FILE = 'training_data/data'
+NCLASSES  = 4
+
+################################################################################
+#
+#                                LOADING DATA
+#
+################################################################################
+import numpy as np
+
+def loadData(classnumber):
+    '''
+        This function loads the training data for a class.
+
+        =======
+        PARAMS
+        =======
+
+         classnumber : int
+              The number that represents the class.
+
+        =======
+        RETURNS
+        =======
+
+         labels      : list of int
+              A list containing the class of the elements loaded.
+
+         data        : list of floats
+              A list containing the recorded data for that class.
+    '''
+    print(f'[INFO] Loading data for class {classnumber}')
+    # Reading the file
+    with open(DATA_FILE+str(classnumber)+'.txt', 'r') as f:
+        bkgd = f.readlines()
+        f.close()
+
+    # Removing unnecessary symbols
+    lines = [line.replace('[', '').replace(']',',').replace('\n','').split(',') for line in bkgd]
+
+    # Getting the individual strings of just floats
+    data = []
+    labels = []
+    label = np.zeros(NCLASSES)
+    label[classnumber] = 1.0
+    for  idx, line in enumerate(lines):
+        data.append([])
+        for val in line:
+            data[idx].append(val)
+        data[idx] = list(filter(None, data[idx]))
+        # Creates an array with class label
+        labels.append(label)
+
+    # Converting them to floats
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            data[i][j]=float(data[i][j])
+    print(f'[INFO] Loaded {len(labels)} elements')
+    return data, labels
+
+y = []
+X = []
+
+# Loading the data
+for i in range(NCLASSES):
+    tX, ty = loadData(i)
+    y.extend(ty)
+    X.extend(tX)
+
+################################################################################
+#
+#               SPLITTING DATA INTO TRAIN AND TEST SETS
+#
+################################################################################
+from sklearn.model_selection import train_test_split
+
+RANDOM_SEED = 32
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.75, random_state=RANDOM_SEED)
+
+################################################################################
+#
+#                            BUILDING THE MODEL
+#
+################################################################################
+import tensorflow as tf
+
+MODEL = 'models/modelv1.hdf5'
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input((len(X_train[0]), )),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(800, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(400, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(NCLASSES, activation='softmax')
+])
+
+# Show the summary of the model
+model.summary()
+
+# What to save
+cp_callback = tf.keras.callbacks.ModelCheckpoint( MODEL,
+                                                  verbose=1,
+                                                  save_weights_only=False
+                                                 )
+# Early stop
+es_callback = tf.keras.callbacks.EarlyStopping(patience = 20, verbose=1)
+model.compile( optimizer = 'adam',
+               loss='categorical_crossentropy',
+               metrics=['accuracy']
+              )
+
+################################################################################
+#
+#                            TRAINING THE MODEL
+#
+################################################################################
+
+model.fit( np.array(X_train),
+           np.array(y_train),
+           epochs=1000,
+           batch_size=128,
+           validation_data=(np.array(X_test),np.array(y_test)),
+           callbacks=[cp_callback, es_callback]
+           )
